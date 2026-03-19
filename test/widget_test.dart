206072dd6cf72487bc987c1571ad523a6e7997e7
Sean Kuzniak <sean.kuzniak@gmail.com>
2026-03-18 20:15:00 -0400

    Initial Flutter scaffold for Linux desktop target

---
+++ test/widget_test.dart
@@ -0,0 +1,8 @@
+// Placeholder test. Smoke testing is done by running the app on Linux desktop.
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('placeholder', () {
+    expect(1 + 1, 2);
+  });
+}
